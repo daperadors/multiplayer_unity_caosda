0ff8e2071d3591c0b3ab8895956d8d9adda3d4db
david <david@adderit.es>
2023-01-28 14:16:33 +0100

    pool physics

--- Assets/Scripts/Game/DragAndShoot.cs
+++ Assets/Scripts/Game/DragAndShoot.cs
@@ -15,29 +15,40 @@ public class DragAndShoot : MonoBehaviour
     private Vector3 m_EndPoint;
     void Start()
     {
+        Physics2D.gravity = Vector2.zero;
         m_TrajLine = GetComponent<TrajectoryLine>();
         m_Rigidbody = GetComponent<Rigidbody2D>();
         m_Camera = Camera.main;
     }
     private void OnMouseDown()
     {
-        m_StartPoint = m_Camera.ScreenToWorldPoint(Input.mousePosition);
-        m_StartPoint.z = 15;
+        if (m_Rigidbody.velocity == Vector2.zero)
+        {
+            m_StartPoint = m_Camera.ScreenToWorldPoint(Input.mousePosition);
+            m_StartPoint.z = 15;
+        }
     }
     private void OnMouseDrag()
     {
-        Vector3 currentPoint = m_Camera.ScreenToWorldPoint(Input.mousePosition);
-        m_StartPoint.z = 15;
-        m_TrajLine.RenderLine(m_StartPoint, currentPoint);
+        if (m_Rigidbody.velocity == Vector2.zero)
+        {
+            Vector3 currentPoint = m_Camera.ScreenToWorldPoint(Input.mousePosition);
+            m_StartPoint.z = 15;
+            m_TrajLine.RenderLine(m_StartPoint, currentPoint);
+        }
     }
     private void OnMouseUp()
     {
-        m_EndPoint = m_Camera.ScreenToWorldPoint(Input.mousePosition);
-        m_EndPoint.z = 15;
+        if (m_Rigidbody.velocity == Vector2.zero)
+        {
+            m_EndPoint = m_Camera.ScreenToWorldPoint(Input.mousePosition);
+            m_EndPoint.z = 15;
 
-        m_Force = new Vector2(Mathf.Clamp(m_StartPoint.x - m_EndPoint.x, m_MinPower.x, m_MaxPower.x),
-                              Mathf.Clamp(m_StartPoint.y - m_EndPoint.y, m_MinPower.y, m_MaxPower.y));
-        m_Rigidbody.AddForce(m_Force * m_PowerBall * m_Impulse, ForceMode2D.Impulse);
-        m_TrajLine.EndLine();
+            m_Force = new Vector2(Mathf.Clamp(m_StartPoint.x - m_EndPoint.x, m_MinPower.x, m_MaxPower.x),
+                                  Mathf.Clamp(m_StartPoint.y - m_EndPoint.y, m_MinPower.y, m_MaxPower.y));
+            m_Rigidbody.AddForce(m_Force * m_PowerBall * m_Impulse, ForceMode2D.Impulse);
+            m_TrajLine.EndLine();
+        }
     }
 }
+
